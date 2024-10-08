44f7a23352c9b75979d5158beb99dbc4d1261945
mohamed.ehab <mohamed.ehab@mytigate.com>
2024-10-08 11:48:32 +0300

    Add Sidebar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { alpha } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
-import Calender from './components/CalenderView';
-import { Container } from '@mui/material';
+import Calender from './components/CalenderView'
+import Sidebar from './components/Sidebar';
 
 export default function Dashboard() {
   return (
-    <Container>
-      <CssBaseline enableColorScheme />
-      <Box sx={{ display: 'flex' }}>
+      <Box>
         <Box
           component="main"
           sx={(theme) => ({
-            flexGrow: 1,
+            display: 'flex',
             backgroundColor: theme.vars
               ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
               : alpha(theme.palette.background.default, 1),
@@ -22,17 +19,17 @@ export default function Dashboard() {
         >
           <Stack
             spacing={2}
+            direction="row"
             sx={{
-              alignItems: 'center',
               mx: 3,
               pb: 5,
               mt: { xs: 8, md: 0 },
             }}
           >
+            <Sidebar />
             <Calender />
           </Stack>
         </Box>
       </Box>
-    </Container>
   );
 }
